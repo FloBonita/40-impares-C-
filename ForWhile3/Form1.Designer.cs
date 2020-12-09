9583a1a7b24103bd8610ed298f114d9f1e3dd6cd
FloBonita <74161621+FloBonita@users.noreply.github.com>
2020-12-09 19:32:47 -0300

    Versão Final

---
+++ ForWhile3/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ForWhile3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lstShow = new System.Windows.Forms.ListBox();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstShow
+            // 
+            this.lstShow.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstShow.ForeColor = System.Drawing.Color.Teal;
+            this.lstShow.FormattingEnabled = true;
+            this.lstShow.ItemHeight = 26;
+            this.lstShow.Location = new System.Drawing.Point(34, 75);
+            this.lstShow.Name = "lstShow";
+            this.lstShow.Size = new System.Drawing.Size(159, 316);
+            this.lstShow.TabIndex = 0;
+            this.lstShow.SelectedIndexChanged += new System.EventHandler(this.lst1_SelectedIndexChanged);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.ForeColor = System.Drawing.Color.LightSeaGreen;
+            this.lbl1.Location = new System.Drawing.Point(22, 9);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(642, 38);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "40 primeiros números ímpares maiores que zero";
+            // 
+            // btnShow
+            // 
+            this.btnShow.BackColor = System.Drawing.Color.Teal;
+            this.btnShow.Font = new System.Drawing.Font("Comic Sans MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnShow.ForeColor = System.Drawing.Color.White;
+            this.btnShow.Location = new System.Drawing.Point(237, 338);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(132, 53);
+            this.btnShow.TabIndex = 2;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = false;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(676, 417);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lstShow);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Exercicio3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstShow;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ ForWhile3/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForWhile3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lst1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+
+            int i;
+            for (i=1; i <=40; i++)
+            {
+               if (i % 2 != 0)
+                {
+                    lstShow.Items.Add(i);
+                }               
+            }
+            
+            
+            //Faça um programa que calcule e imprima os 40 primeiros números ímpares maiores que zero.
+
+        }
+    }
+}
